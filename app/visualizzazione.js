656364d7ee8588f44663ef562e642ebc80955dfb
TLZ2000 <thomas@cnt3.com>
2022-05-18 21:17:07 +0200

    AggiustamentiMerge#2

--- app/visualizzazione.js
+++ app/visualizzazione.js
@@ -7,27 +7,28 @@ const Category = require('./models/category');
 const User = require('./models/user');
 const Housing = require('./models/housing');
 
-/**
- * @openapi
- * /api/v1/visualizzazione/eventList:
- *   get:
- *     tags:
- *       -  eventVisualization
- *     description: Gets the list of all events
- *     summary: View all events
- *     responses:
- *       200:
- *         description: Collection of events in JSON format
- *         schema:
- *           type: object
- *           properties:
- *             id:
- *               type: string
- *               description: string that identifies the event
- *             title:
- *               type: string
- *               description: title of the event
- */
+ /**
+  * @openapi
+  * /api/v1/visualizzazione/eventList:
+  *   get:
+  *     description: Gets the list of all events
+  *     summary: View all events
+  *     tags:
+  *       - eventVisualization
+  *     responses:
+  *       200:
+  *         description: Collection of events in JSON format
+  *         content:
+  *           application/json:
+  *             schema:
+  *               properties:
+  *                 id:
+  *                   type: string
+  *                   description: string that identifies the event
+  *                 title:
+  *                   type: string
+  *                   description: title of the event
+  */
 router.get('/eventList', async (req, res) => {
     let events = await Event.find().exec();
     let eventsList = events.map((eventItem) => {return{id:eventItem._id, title:eventItem.titolo};})
@@ -39,24 +40,24 @@ router.get('/eventList', async (req, res) => {
 /**
  * @openapi
  * /api/v1/visualizzazione/housingList:
- *   tag: dataVisualization
  *   get:
- *     tags:
- *       -  housingVisualization
  *     description: Gets the list of all housings
  *     summary: View all housings
+ *     tags:
+ *       - housingVisualization
  *     responses:
  *       200:
  *         description: Collection of housings in JSON format
- *         schema:
- *           type: object
- *           properties:
- *             id:
- *               type: string
- *               description: string that identifies the housing
- *             title:
- *               type: string
- *               description: title of the housing
+ *         content:
+ *           application/json:
+ *             schema:
+ *               properties:
+ *                 id:
+ *                   type: string
+ *                   description: string that identifies the housing
+ *                 title:
+ *                   type: string
+ *                   description: title of the housing
  */
 router.get('/housingList', async (req, res) => {
     let housings = await Housing.find().exec();
@@ -64,82 +65,87 @@ router.get('/housingList', async (req, res) => {
     res.status(200).json(housingsList);
 });
 
-/**
- * @openapi
- * /api/v1/visualizzazione/event:
- *   get:
- *     tags:
- *       -  eventVisualization
- *     description: Gets the details of a specific event
- *     summary: Details of one event
- *     parameters:
- *       - in: query
- *         name: id
- *         type: string
- *         description: The id of the specific event you want to get the details
- *     responses:
- *       200:
- *         description: Details of an event in JSON format
- *         schema:
- *           type: object
- *           properties:
- *             title:
- *               type: string
- *               description: title of the event
- *             description:
- *               type: string
- *               description: description of the event
- *             initDate:
- *               type: string
- *               description: date of the beginning of the event in ISO8601 format
- *             finlDate:
- *               type: string
- *               description: date of the ending of the event in ISO8601 format
- *             address:
- *               type: string
- *               description: address of the event
- *             city:
- *               type: string
- *               description: city where the event takes place
- *             seatsAvailable:
- *               type: integer
- *               description: seats still available for the event
- *             seatsOccupied:
- *               type: number
- *               description: seats already occupied for the event
- *             category:
- *               type: number
- *               description: seats already occupied for the event
- *             creatorName:
- *               type: string
- *               description: the name of the user who created the event
- *             creatorSurname:
- *               type: string
- *               description: the surname of the user who created the event
- *             creatorEmail:
- *               type: string
- *               description: the email of the user who created the event
- *       404:
- *         description: Some data are not found on the DB
- *         schema:
- *           type: object
- *           properties:
- *             success:
- *               type: boolean
- *             message:
- *               type: string
- *               description: specifications regarding the specific error
- *       400:
- *         description: The Id query parameter is not present or do not respect the MongoDB format
- *         schema:
- *           type: object
- *           properties:
- *             success:
- *               type: boolean
- *             message:
- *               type: string
- *               description: specifications regarding the specific error
- */
+
+
+ /**
+  * @openapi
+  * /api/v1/visualizzazione/event:
+  *   get:
+  *     description: Gets the details of a specific event
+  *     summary: Details of one event
+  *     tags:
+  *       - eventVisualization
+  *     parameters:
+  *       - in: query
+  *         name: id
+  *         type: string
+  *         description: The id of the specific event you want to get the details
+  *     responses:
+  *       200:
+  *         description: Details of an event in JSON format
+  *         content:
+  *           application/json:
+  *             schema:
+  *               properties:
+  *                 title:
+  *                   type: string
+  *                   description: title of the event
+  *                 description:
+  *                   type: string
+  *                   description: description of the event
+  *                 initDate:
+  *                   type: string
+  *                   description: date of the beginning of the event in ISO8601 format
+  *                 finlDate:
+  *                   type: string
+  *                   description: date of the ending of the event in ISO8601 format
+  *                 address:
+  *                   type: string
+  *                   description: address of the event
+  *                 city:
+  *                   type: string
+  *                   description: city where the event takes place
+  *                 seatsAvailable:
+  *                   type: integer
+  *                   description: seats still available for the event
+  *                 seatsOccupied:
+  *                   type: number
+  *                   description: seats already occupied for the event
+  *                 category:
+  *                   type: number
+  *                   description: seats already occupied for the event
+  *                 creatorName:
+  *                   type: string
+  *                   description: the name of the user who created the event
+  *                 creatorSurname:
+  *                   type: string
+  *                   description: the surname of the user who created the event
+  *                 creatorEmail:
+  *                   type: string
+  *                   description: the email of the user who created the event
+  *       404:
+  *         description: Some data are not found on the DB
+  *         content:
+  *           application/json:
+  *             schema:
+  *               properties:
+  *                 success:
+  *                   type: boolean
+  *                 message:
+  *                   type: string
+  *                   description: specifications regarding the specific error
+  *       400:
+  *         description: The Id query parameter is not present or do not respect the MongoDB format
+  *         content:
+  *           application/json:
+  *             schema:
+  *               properties:
+  *                 success:
+  *                   type: boolean
+  *                 message:
+  *                   type: string
+  *                   description: specifications regarding the specific error
+  */
 router.get('/event', async (req, res) => {
     // Controlla che sia effettivamente presente il parametro id
     if (!req.query.id) {
@@ -206,73 +212,77 @@ router.get('/event', async (req, res) => {
     res.status(200).json(finalResponse);
 });
 
-/**
- * @openapi
- * /api/v1/visualizzazione/housing:
- *   get:
- *     tags:
- *       -  housingVisualization
- *     description: Gets the details of a specific housing
- *     summary: Details of one housing
- *     parameters:
- *       - in: query
- *         name: id
- *         type: string
- *         description: The id of the specific housing you want to get the details
- *     responses:
- *       200:
- *         description: Details of a housing in JSON format
- *         schema:
- *           type: object
- *           properties:
- *             title:
- *               type: string
- *               description: title of the housing
- *             description:
- *               type: string
- *               description: description of the housing
- *             initDate:
- *               type: string
- *               description: date of the beginning of the housing availability slot in ISO8601 format
- *             finlDate:
- *               type: string
- *               description: date of the ending of the housing availability slot in ISO8601 format
- *             address:
- *               type: string
- *               description: address of the housing
- *             city:
- *               type: string
- *               description: city where the housing is located
- *             creatorName:
- *               type: string
- *               description: the name of the user who created the housing
- *             creatorSurname:
- *               type: string
- *               description: the surname of the user who created the housing
- *             creatorEmail:
- *               type: string
- *               description: the email of the user who created the housing
- *       404:
- *         description: Some data are not found on the DB
- *         schema:
- *           type: object
- *           properties:
- *             success:
- *               type: boolean
- *             message:
- *               type: string
- *               description: specifications regarding the specific error
- *       400:
- *         description: The Id query parameter is not present or do not respect the MongoDB format
- *         schema:
- *           type: object
- *           properties:
- *             success:
- *               type: boolean
- *             message:
- *               type: string
- *               description: specifications regarding the specific error
- */
+
+ /**
+  * @openapi
+  * /api/v1/visualizzazione/housing:
+  *   get:
+  *     description: Gets the details of a specific housing
+  *     summary: Details of one housing
+  *     tags:
+  *       - housingVisualization
+  *     parameters:
+  *       - in: query
+  *         name: id
+  *         type: string
+  *         description: The id of the specific housing you want to get the details
+  *     responses:
+  *       200:
+  *         description: Details of a housing in JSON format
+  *         content:
+  *           application/json:
+  *             schema:
+  *               properties:
+  *                 title:
+  *                   type: string
+  *                   description: title of the housing
+  *                 description:
+  *                   type: string
+  *                   description: description of the housing
+  *                 initDate:
+  *                   type: string
+  *                   description: date of the beginning of the housing availability slot in ISO8601 format
+  *                 finlDate:
+  *                   type: string
+  *                   description: date of the ending of the housing availability slot in ISO8601 format
+  *                 address:
+  *                   type: string
+  *                   description: address of the housing
+  *                 city:
+  *                   type: string
+  *                   description: city where the housing is located
+  *                 creatorName:
+  *                   type: string
+  *                   description: the name of the user who created the housing
+  *                 creatorSurname:
+  *                   type: string
+  *                   description: the surname of the user who created the housing
+  *                 creatorEmail:
+  *                   type: string
+  *                   description: the email of the user who created the housing
+  *       404:
+  *         description: Some data are not found on the DB
+  *         content:
+  *           application/json:
+  *             schema:
+  *               properties:
+  *                 success:
+  *                   type: boolean
+  *                 message:
+  *                   type: string
+  *                   description: specifications regarding the specific error specific error
+  *       400:
+  *         description: The Id query parameter is not present or do not respect the MongoDB format
+  *         content:
+  *           application/json:
+  *             schema:
+  *               properties:
+  *                 success:
+  *                   type: boolean
+  *                 message:
+  *                   type: string
+  *                   description: specifications regarding the specific error
+  */
 router.get('/housing', async (req, res) => {
     // Controlla che sia effettivamente presente il parametro id
     if (!req.query.id) {
