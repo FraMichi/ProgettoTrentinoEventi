1486f7a64c2e57a681a4d2199559d849da920d1c
TLZ2000 <thomas@cnt3.com>
2022-05-22 16:38:47 +0200

    Update di tokenChecker.js

--- app/tokenChecker.js
+++ app/tokenChecker.js
@@ -1,25 +1,21 @@
 const jwt = require("jsonwebtoken");
 
-const tokenChecker = function(req, res, specToken){
-    // ottieni il token dello user loggato
-    let token = specToken;
-
-    // se non c'è il token
+const tokenChecker = function(req, res, token){
+    // Se non c'è il token
     if (!token){
-        // ritorna errore
-        //res.status(401).json({success:false,message:'No token provided'})
+        // Ritorna errore
         return false;
     }
 
-    // decodifica il token e controllane la validità
+    // Decodifica il token e controllane la validità
     jwt.verify(token, process.env.TOKEN_SECRET, function(err, decoded) {
         if (err){
-            //res.status(401).json({success:false,message:'Token not valid'});
+            // Ritorna errore
             return false;
         } else {
-            // if everything is good, save in req object for use in other routes
-            //res.status(401).json({success:false,message:'Token valid'});  //DEBUG
+            // Se tutto va bene, imposta il parametro nella request
             req.loggedUser = decoded;
+            return true;
         }
     });
 };
