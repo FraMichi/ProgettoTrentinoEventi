f033d6ed5b38b9aa7d4d1bf7fef38416d3943399
FraMichi <104912310+FraMichi@users.noreply.github.com>
2022-05-28 10:29:58 +0200

    Update elimination.js

--- app/v2/elimination.js
+++ app/v2/elimination.js
@@ -506,7 +506,7 @@ router.delete('/deleteHousingSubscription', async (req, res) => {
     }
 
     // Se user loggato controlla se registrato ad alloggio specifico
-    let prenotations = await HousingSubscription.findOne({idAlloggio: housingId, idTurista: req.loggedUser.id, dataInizio: initDate.toISOString(), dataFine: finlDate.toISOString()});
+    let prenotations = await HousingSubscription.findOne({idAlloggio: housingId, idTurista: req.loggedUser.id});
 
     // Se non prenotato, invia un messaggio di errore
     if (!prenotations) {
@@ -518,7 +518,7 @@ router.delete('/deleteHousingSubscription', async (req, res) => {
   	}
 
   	// Elimina l'iscrizione dell'utente dall'evento
-  	await HousingSubscription.deleteOne({ idAlloggio: housingId, idTurista: req.loggedUser.id, dataInizio: initDate.toISOString(), dataFine: finlDate.toISOString() });
+  	await HousingSubscription.deleteOne({ idAlloggio: housingId, idTurista: req.loggedUser.id });
 
 
   	res.status(200).json({
