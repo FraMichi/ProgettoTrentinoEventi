64bbfd6ce366f6a4f9250327a1ba93b19aac8558
FraMichi <f.michieletto@studenti.unitn.it>
2022-05-30 20:30:47 +0200

    aggiornamento app.js

--- app/app.js
+++ app/app.js
@@ -15,6 +15,7 @@ const visualizzazioneV2 = require('./v2/visualizzazione.js');
 const elimination = require("./v2/elimination.js");
 const visualizzazioneFiltrata = require("./v2/visualizzazioneFiltrata.js");
 const review = require("./v2/review.js");
+const deleteReview = require("./v2/deleteReview.js");
 
 var fs = require('fs');
 const app = express();
@@ -35,7 +36,7 @@ const swaggerOptions = {
       }
     }
   },
-  apis: ['./app/v1/authentication.js', './app/v1/visualizzazione.js', './app/v1/eventSubscription.js', './app/v1/housingSubscription.js', './app/v1/accommodation.js', './app/v1/event.js', './app/v2/getCreatedEntries.js', './app/v2/visualizzazione.js', './app/v2/elimination.js', './app/v2/visualizzazioneFiltrata.js', './app/v2/review.js'] // files containing annotations as above
+  apis: ['./app/v1/authentication.js', './app/v1/visualizzazione.js', './app/v1/eventSubscription.js', './app/v1/housingSubscription.js', './app/v1/accommodation.js', './app/v1/event.js', './app/v2/getCreatedEntries.js', './app/v2/visualizzazione.js', './app/v2/elimination.js', './app/v2/visualizzazioneFiltrata.js', './app/v2/review.js', './app/v2/deleteReview.js'] // files containing annotations as above
 };
 
 // Si crea il documento della documentazione
@@ -65,6 +66,7 @@ app.use('/api/v2/getCreatedEntries', getCreatedEntries);
 app.use('/api/v2/visualizzazione', visualizzazioneV2);
 app.use('/api/v2/elimination', elimination);
 app.use('/api/v2/review', review);
+app.use('/api/v2/deleteReview', deleteReview);
 
 app.use('/', express.static('static'));
 
