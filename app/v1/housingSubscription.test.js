0af361c7102fce92d4220734c135b97139edc9a3
TLZ2000 <thomas@cnt3.com>
2022-06-07 01:09:52 +0200

    Final fix

--- app/v1/housingSubscription.test.js
+++ app/v1/housingSubscription.test.js
@@ -68,34 +68,28 @@ describe('POST /api/v1/housingSubscription/getHousingSlots', () => {
         UTENTE NON LOGGATO
     */
     // id non conforme
-    test('POST /api/v1/housingSubscription/getHousingSlots con id non conforme', () => {
+    test('POST /api/v1/housingSubscription/getHousingSlots con id non conforme e token non valido', () => {
         return request(app).post('/api/v1/housingSubscription/getHousingSlots')
         .send({token: tokenNoV, id:"62838c1f3sdfsdfsdgsdgsdba701dd200682e9"}).set('Accept', 'application/json')
-        .expect(400, {
-            success: false,
-            message: "MongoDBFormatException"
-        });
+        .expect(400, {success: false, message: "MongoDBFormatException"});
     });
 
     // id non presente
-    test('POST /api/v1/housingSubscription/getHousingSlots con id alloggio assente', () => {
+    test('POST /api/v1/housingSubscription/getHousingSlots con id alloggio assente e token non valido', () => {
         return request(app).post('/api/v1/housingSubscription/getHousingSlots')
         .send({token: tokenNoV}).set('Accept', 'application/json')
         .expect(400, {success: false, message:'MissingHousing' });
     });
 
     // alloggio non esistente
-    test('POST /api/v1/housingSubscription/getHousingSlots con alloggio non esistente', () => {
+    test('POST /api/v1/housingSubscription/getHousingSlots con alloggio non esistente e token non valido', () => {
         return request(app).post('/api/v1/housingSubscription/getHousingSlots')
         .send({token: tokenNoV, id:"627fdec095b0619bf9e00000"}).set('Accept', 'application/json')
-        .expect(404, {
-            success: false,
-            message: "HousingNotFound"
-        });
+        .expect(404, {success: false, message: "HousingNotFound"});
     });
 
     // alloggio esistente
-    test('POST /api/v1/housingSubscription/getHousingSlots con alloggio esistente', () => {
+    test('POST /api/v1/housingSubscription/getHousingSlots con alloggio esistente e token non valido', () => {
         return request(app).post('/api/v1/housingSubscription/getHousingSlots')
         .send({token: tokenNoV, id:"627fdec095b0619bf9e97717"}).set('Accept', 'application/json')
         .expect(200, [
@@ -140,24 +134,25 @@ describe('POST /api/v1/housingSubscription/getHousingSlots', () => {
     test('POST /api/v1/housingSubscription/getHousingSlots con id non conforme', () => {
         return request(app).post('/api/v1/housingSubscription/getHousingSlots')
         .send({token: tokenVal, id:"62838c1f3sdfsdfsdgsdgsdba701dd200682e9"}).set('Accept', 'application/json')
-        .expect(400, {
-            success: false,
-            message: "MongoDBFormatException"
-        });
+        .expect(400, {success: false, message: "MongoDBFormatException"});
+    });
+
+    // id non presente
+    test('POST /api/v1/housingSubscription/getHousingSlots con id alloggio assente', () => {
+        return request(app).post('/api/v1/housingSubscription/getHousingSlots')
+        .send({token: tokenVal}).set('Accept', 'application/json')
+        .expect(400, {success: false, message:'MissingHousing' });
     });
 
     // alloggio non esistente
     test('POST /api/v1/housingSubscription/getHousingSlots con alloggio non esistente', () => {
         return request(app).post('/api/v1/housingSubscription/getHousingSlots')
         .send({token: tokenVal, id:"627fdec095b0619bf9e00000"}).set('Accept', 'application/json')
-        .expect(404, {
-            success: false,
-            message: "HousingNotFound"
-        });
+        .expect(404, {success: false, message: "HousingNotFound"});
     });
 
-    // alloggio non esistentenp
-    test('POST /api/v1/housingSubscription/getHousingSlots', () => {
+    // alloggio eistente
+    test('POST /api/v1/housingSubscription/getHousingSlots con alloggio esistente', () => {
         return request(app).post('/api/v1/housingSubscription/getHousingSlots')
         .send({token: tokenVal, id:"627fdec095b0619bf9e97717"}).set('Accept', 'application/json')
         .expect(200, [
@@ -249,37 +244,31 @@ describe('POST /api/v1/housingSubscription/subscribeHousing', () => {
     test('POST /api/v1/housingSubscription/subscribeHousing con ordine data inizio/finale scorretto', () => {
         return request(app).post('/api/v1/housingSubscription/subscribeHousing')
         .send({
-            token: tokenNoV,
-            housingId:"62838c2e9",
+            token: tokenVal,
+            housingId:"627fdec095b0619bf9e97717",
             initDate: "2022-07-20T00:00:00.000Z",
             finlDate: "2022-06-17T00:00:00.000Z"
         }).set('Accept', 'application/json')
-        .expect(200, {
-            success: false,
-            message: "BadDateOrder"
-        });
+        .expect(200, {success: false, message: "BadDateOrder"});
     });
 
     // id non conforme
     test('POST /api/v1/housingSubscription/subscribeHousing con id non conforme', () => {
         return request(app).post('/api/v1/housingSubscription/subscribeHousing')
         .send({
-            token: tokenNoV,
+            token: tokenVal,
             housingId:"62838c2e9",
             initDate: "2022-06-20T00:00:00.000Z",
             finlDate: "2022-07-17T00:00:00.000Z"
         }).set('Accept', 'application/json')
-        .expect(400, {
-            success: false,
-            message: "MongoDBFormatException"
-        });
+        .expect(400, {success: false, message: "MongoDBFormatException"});
     });
 
     // id assente
     test('POST /api/v1/housingSubscription/subscribeHousing con id assente', () => {
         return request(app).post('/api/v1/housingSubscription/subscribeHousing')
         .send({
-            token: tokenNoV,
+            token: tokenVal,
             initDate: "2022-06-20T00:00:00.000Z",
             finlDate: "2022-07-17T00:00:00.000Z"
         }).set('Accept', 'application/json')
@@ -287,10 +276,10 @@ describe('POST /api/v1/housingSubscription/subscribeHousing', () => {
     });
 
     // date assenti
-    test('POST /api/v1/housingSubscription/subscribeHousing con id assente', () => {
+    test('POST /api/v1/housingSubscription/subscribeHousing con date assenti', () => {
         return request(app).post('/api/v1/housingSubscription/subscribeHousing')
         .send({
-            token: tokenNoV,
+            token: tokenVal,
             housingId:"627fdec095b0619bf9e97717"
         }).set('Accept', 'application/json')
         .expect(400, {success: false, message:"MissingParameters"});
@@ -305,10 +294,7 @@ describe('POST /api/v1/housingSubscription/subscribeHousing', () => {
             initDate: "2022-06-20T00:00:00.000Z",
             finlDate: "2022-07-17T00:00:00.000Z"
         }).set('Accept', 'application/json')
-        .expect(401, {
-            success: false,
-            message: "TokenNotValid"
-        });
+        .expect(401, {success: false, message: "TokenNotValid"});
     });
 
     // housing non esistente
@@ -320,10 +306,7 @@ describe('POST /api/v1/housingSubscription/subscribeHousing', () => {
             initDate: "2022-06-20T00:00:00.000Z",
             finlDate: "2022-07-17T00:00:00.000Z"
         }).set('Accept', 'application/json')
-        .expect(404, {
-            success: false,
-            message: "HousingNotFound"
-        });
+        .expect(404, {success: false, message: "HousingNotFound"});
     });
 
     // initDate non coerente
@@ -335,10 +318,7 @@ describe('POST /api/v1/housingSubscription/subscribeHousing', () => {
             initDate: "2022-05-17T00:00:00.000Z",
             finlDate: "2022-08-02T00:00:00.000Z"
         }).set('Accept', 'application/json')
-        .expect(200, {
-            success: false,
-            message: "BadDateOffset"
-        });
+        .expect(200, {success: false, message: "BadDateOffset"});
     });
 
     // finlDate non coerente
@@ -350,14 +330,11 @@ describe('POST /api/v1/housingSubscription/subscribeHousing', () => {
             initDate: "2022-05-18T00:00:00.000Z",
             finlDate: "2022-08-03T00:00:00.000Z"
         }).set('Accept', 'application/json')
-        .expect(200, {
-            success: false,
-            message: "BadDateOffset"
-        });
+        .expect(200, {success: false, message: "BadDateOffset"});
     });
 
     // sovrapposizione data iniziale slot prenotato
-    test('POST /api/v1/housingSubscription/subscribeHousing con sovrapposizione della data iniziale con slot antecedente', () => {
+    test('POST /api/v1/housingSubscription/subscribeHousing con sovrapposizione slot', () => {
         return request(app).post('/api/v1/housingSubscription/subscribeHousing')
         .send({
             token: tokenVal,
@@ -365,25 +342,7 @@ describe('POST /api/v1/housingSubscription/subscribeHousing', () => {
             initDate: "2022-07-16T00:00:00.000Z",
             finlDate: "2022-07-19T00:00:00.000Z"
         }).set('Accept', 'application/json')
-        .expect(200, {
-            success: false,
-            message: "DateSlotOverlap"
-        });
-    });
-
-    // sovrapposizione data finale slot prenotato
-    test('POST /api/v1/housingSubscription/subscribeHousing con sovrapposizione della data finale con slot successivo', () => {
-        return request(app).post('/api/v1/housingSubscription/subscribeHousing')
-        .send({
-            token: tokenVal,
-            housingId:"627fdec095b0619bf9e97717",
-            initDate: "2022-07-10T00:00:00.000Z",
-            finlDate: "2022-07-12T00:00:00.000Z"
-        }).set('Accept', 'application/json')
-        .expect(200, {
-            success: false,
-            message: "DateSlotOverlap"
-        });
+        .expect(200, {success: false, message: "DateSlotOverlap"});
     });
 
     // creazione prenotazione
@@ -395,9 +354,6 @@ describe('POST /api/v1/housingSubscription/subscribeHousing', () => {
             initDate: "2022-07-10T00:00:00.000Z",
             finlDate: "2022-07-11T00:00:00.000Z"
         }).set('Accept', 'application/json')
-        .expect(201, {
-            success: true,
-            message: "UserSubscribed"
-        });
+        .expect(201, {success: true, message: "UserSubscribed"});
     });
 });
